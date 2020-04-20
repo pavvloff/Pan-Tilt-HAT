0614ff6c76b45ee7c0f9da011417d024927fb83e
Anton Pavlov <pavvloff@gmail.com>
2020-04-20 21:40:04 +0200

    Fixing bugs and removing the motor stop command

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from pth.camera import runCameraView
 def main():
 	parser = argparse.ArgumentParser(description='Run the camera software.')
 	parser.add_argument('-s', '--static', type=str, help='Path to static files folder', default='.')
+        parser.add_argument('-f', '--freq', type=int, help='Motor update frequency', default = 50)
 	args = parser.parse_args()
 
-	runCameraView(root = args.static)
+	runCameraView(root = args.static, freq = args.freq)
 
 if __name__ == '__main__':
 	main()
--- pth/camera.py
+++ pth/camera.py
@@ -9,11 +9,10 @@ import os.path as path
 HORIZONTAL_MOTOR = 1
 VERTICAL_MOTOR = 0
 I2C_ADDRESS = 0x40
-FREQUENCY = 50
 
 class Stepper:
   def __init__(self, min_val = 400, max_val = 1800, max_speed = 3, acceleration = 0.1):
-    assert min_val > 0
+    assert min_val >= 0
     assert max_val > min_val
     assert max_speed > 0
     assert acceleration > 0
@@ -49,12 +48,12 @@ class Stepper:
 
 
 class PlatformControl(threading.Thread):
-  def __init__(self, freq):
-    super(ClassName, self).__init__()
+  def __init__(self, freq = 50, min_v = 0, max_v = 2500, min_h = 0, max_h = 2500):
+    super(PlatformControl, self).__init__()
     self.freq = freq
     self.sleep_interval = 1.0 / freq
-    self.horizontal = Stepper()
-    self.vertical = Stepper()
+    self.horizontal = Stepper(min_val = min_h, max_val = max_h)
+    self.vertical = Stepper(min_val = min_v, max_val = max_v)
     self.pwm = PCA9685.PCA9685(I2C_ADDRESS)
     self.pwm.setPWMFreq(self.freq)
     self.moving = False
@@ -64,9 +63,9 @@ class PlatformControl(threading.Thread):
 
   def processCommand(self):
     if self.command == 'up':
-      self.vertical.more()
-    elif self.command == 'down':
       self.vertical.less()
+    elif self.command == 'down':
+      self.vertical.more()
     else:
       self.vertical.slowDown()
 
@@ -88,12 +87,9 @@ class PlatformControl(threading.Thread):
       self.startMotor()
 
     if h_move:
-      self.pwm.setServoPulse(HORIZONTAL_MOTOR, round(self.horizontal.value))
+      self.pwm.setServoPulse(HORIZONTAL_MOTOR, int(round(self.horizontal.value)))
     if v_move:
-      self.pwm.setServoPulse(VERTICAL_MOTOR, round(self.vertical.value))
-
-    if not (v_move or h_move):
-      self.stopMotor()
+      self.pwm.setServoPulse(VERTICAL_MOTOR, int(round(self.vertical.value)))
 
   def stopMotor(self):
     if self.moving:
@@ -116,32 +112,32 @@ class PlatformControl(threading.Thread):
 
 platform = None
 
-def runCameraView(port = 8001, root = '.'):
+def runCameraView(port = 8001, root = '.', freq = 50):
 
   global platform
 
-  platform = PlatformControl(FREQUENCY)
+  platform = PlatformControl(freq)
   platform.start()
 
   app = bottle.Bottle()
 
   @app.get('/')
   def index():
-    return bottle.static_file('/autodeploy/index.html', root=path.join(root,'/static/'))
+    return bottle.static_file('/static/camera/index.html', root=root)
     
   @app.route('/static/<filepath:path>')
   def server_static(filepath):
-    return bottle.static_file(filepath, root=path.join(root,'/static/'))
+    return bottle.static_file(path.join('/static/', filepath), root=root)
 
   @app.route('/fonts/<filepath:path>')
   def server_fonts(filepath):
-    return bottle.static_file(filepath, root=path.join(root,'/static/fonts/'))
+    return bottle.static_file(path.join('/static/fonts/', filepath), root=root)
     
   @app.post('/cmd')
   def cmd():
     global platform
-    platform.command = request.body.read().decode()
-    if platform.command not in frozenset('stop', 'up', 'down', 'left', 'right', 'exit'):
+    platform.command = bottle.request.body.read().decode()
+    if platform.command not in frozenset(['stop', 'up', 'down', 'left', 'right', 'exit']):
       platform.command = 'stop'
     print(platform.command)
     return 'OK'
