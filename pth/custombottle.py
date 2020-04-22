3d3fb93c7b32e3f2d8cccebc3e75bfa2f4574485
Anton Pavlov <pavvloff@gmail.com>
2020-04-22 08:59:42 +0200

    Changing the auto restart logic

--- pth/custombottle.py
+++ pth/custombottle.py
@@ -1,18 +1,25 @@
 from bottle import ServerAdapter
+from threading import Timer
+
+def stopServer(server = None):
+  print('stopping server now...')
+  server.server_close()
+  print('closed')
 
 class StoppableServer(ServerAdapter):
-    server = None
+  server = None
 
-    def run(self, handler):
-        from wsgiref.simple_server import make_server, WSGIRequestHandler
-        if self.quiet:
-            class QuietHandler(WSGIRequestHandler):
-                def log_request(*args, **kw): pass
-            self.options['handler_class'] = QuietHandler
-        self.server = make_server(self.host, self.port, handler, **self.options)
-        self.server.serve_forever()
+  def run(self, handler):
+    from wsgiref.simple_server import make_server, WSGIRequestHandler
+    if self.quiet:
+      class QuietHandler(WSGIRequestHandler):
+        def log_request(*args, **kw): pass
+      self.options['handler_class'] = QuietHandler
+    self.server = make_server(self.host, self.port, handler, **self.options)
+    self.server.serve_forever()
 
-    def stop(self):
-    	print('stopping server...')
-        # self.server.server_close() <--- alternative but causes bad fd exception
-        self.server.shutdown()
+  def stop(self):
+    print('stopping server in 0.5...')
+    t = Timer(0.5, stopServer, server=self.server)
+    t.start()
+    
